59b0a53c508723050f4c5c71cad9b72b57399b27
RaviC Rachamalla <rach0022@algonquinlive.com>
2020-02-24 23:01:49 -0500

    added sanitizebody middleware and route stater files

---
+++ startup/database.js
@@ -0,0 +1,14 @@
+const debug = require('debug')('a2:database');
+const mongoose = require('mongoose');
+
+module.exports = () =>{
+    mongoose.connect('mongodb://localhose:27017/mad9124', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        .then(()=> debug('Connected to MongoDB...'))
+        .catch(err =>{
+            debug('Problem connecting to MongoDB', err);
+            process.exit(1);
+        });
+};
