83799b423fc8f036118e9bf45a328d75f86c74ce
RaviC Rachamalla <rach0022@algonquinlive.com>
2020-03-02 14:12:26 -0500

    fixed sanitizebody middleware, forgot to return sanitize function

--- middleware/sanitizeBody.js
+++ middleware/sanitizeBody.js
@@ -1,23 +1,16 @@
 const debug = require('debug')('a2:sanitize');
 const xss = require('xss'); //may have to use let if const dosent work
 
-module.exports = (req,res,next) => {
-    //logic to sanitize the data goes here
-    const{id,_id, ...attributes} = req.body; //strip out the id portion of the request
-    const sanitizedBody = stripTags(attributes);
-
-    req.sanitizedBody = sanitizedBody; //set the new sanitized body without mutating the original data
-    next(); //call the next function in the request loop
-}
-
 const stripTags = payload => {
-    let attributes = {...payload};
+    let attributes = {...payload}; //we copy the payload so as to not mutate it
     for(let key in payload){
         if(attributes[key] instanceof Array){ //to check if the element is an array
             attributes[key] = attributes[key].map(element =>{
                 //either check if its a string or recursively pass it again to strip tags to go down another level
-                let cleanedElement = typeof element === 'string' ? sanitize(element) : stripTags(element);
-                return cleanedElement;
+                // let cleanedElement = typeof element === 'string' ? sanitize(element) : stripTags(element);
+                // return cleanedElement;
+                return typeof element === "string" ? sanitize(element) : stripTags(element)
+
             })
         } else if(attributes[key] instanceof Object){ //if its an object send it down another level recursively
             attributes[key] = stripTags(attributes[key]);
@@ -25,14 +18,23 @@ const stripTags = payload => {
             attributes[key] = sanitize(attributes[key]);
         }
     };
-    debug(attributes);
     return attributes;
 }
 
 const sanitize = sourceString => {
-    sourceString = xss(sourceString, {
+    return xss(sourceString, {
         whitelist: [],
         stripIgnoreTag: true,
         stripIgnoreTagBody: ['script']
     });
+}
+
+module.exports = (req,res,next) => {
+    //logic to sanitize the data goes here
+    const{id,_id, ...attributes} = req.body; //strip out the id portion of the request
+    const sanitizedBody = stripTags(attributes);
+
+    req.sanitizedBody = sanitizedBody; //set the new sanitized body without mutating the original data
+    debug(req.body);
+    next(); //call the next function in the request loop
 }
