8a4791d5f1e16852f694966c9d91c7a8a699194c
RaviC Rachamalla <rach0022@algonquinlive.com>
2020-03-02 15:10:54 -0500

    added skeleton for student router

--- routes/students.js
+++ routes/students.js
@@ -1,1 +1,16 @@
 //routes/students.js
+const router = require('express').Router();
+const debug = require('debug')('a2:StudentRouter');
+const validateStudentId = require('../middleware/validateStudentId.js');
+const Student = require('../models/Student.js');
+
+router.use(':id', validateStudentId);
+
+router.get('/', async (req, res) =>{});
+router.post('/', async (req, res) =>{});
+router.get('/:id', async (req, res) =>{});
+router.patch('/:id', async (req, res) =>{});
+router.put('/:id', async (req, res) =>{});
+router.delete('/:id', async (req, res) =>{});
+
+module.exports = router; 
